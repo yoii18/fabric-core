a60a53d4f17e9a40c88d5d1445506c835a62ee7f
Yoii <119809971+yoii18@users.noreply.github.com>
2026-09-03 22:05:01 +0530

    Committing 2 items from workspace 0dab5c68-6859-469f-bc25-c0fb31cdeb92

--- Notebooks/NB-CI-TEST.Notebook/notebook-content.py
+++ Notebooks/NB-CI-TEST.Notebook/notebook-content.py
@@ -6,14 +6,30 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "environment": {
+# META       "environmentId": "19b664a3-d261-9187-4d1f-6ed55db07bba",
+# META       "workspaceId": "00000000-0000-0000-0000-000000000000"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+""
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
 # META }
 
 # CELL ********************
 
 from common.module import sum
 
-ans = sum(2+3)
+ans = sum(2,3)
 print(ans)
 
 
